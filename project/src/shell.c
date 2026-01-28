c9dd973603705165992c9f80491f48f20e0887bb
a-bakhtine <alice.bakhtine@gmail.com>
2026-01-28 10:08:02 -0500

    Updated batch mode execution

--- project/src/shell.c
+++ project/src/shell.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
-//#include <unistd.h>
+#include <unistd.h>
 #include "shell.h"
 #include "interpreter.h"
 #include "shellmemory.h"
@@ -23,11 +23,16 @@ int main(int argc, char *argv[]) {
     
     //init shell memory
     mem_init();
+    int mode = isatty(STDIN_FILENO); // 1 terminal, 0 else
     while(1) {							
-        printf("%c ", prompt);
+        if (mode) {
+            printf("%c ", prompt);
+        }
         // here you should check the unistd library 
         // so that you can find a way to not display $ in the batch mode
-        fgets(userInput, MAX_USER_INPUT-1, stdin);
+        if (fgets(userInput, MAX_USER_INPUT-1, stdin) == NULL) {
+           break; 
+        }
         errorCode = parseInput(userInput);
         if (errorCode == -1) exit(99);	// ignore all other errors
         memset(userInput, 0, sizeof(userInput));

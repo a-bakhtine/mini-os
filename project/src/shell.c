926cc314050afab502438438380ca1144af6e651
a-bakhtine <alice.bakhtine@gmail.com>
2026-01-29 14:40:42 -0500

    Added command chaining

--- project/src/shell.c
+++ project/src/shell.c
@@ -42,12 +42,10 @@ int main(int argc, char *argv[]) {
 }
 
 int wordEnding(char c) {
-    // You may want to add ';' to this at some point,
-    // or you may want to find a different way to implement chains.
     return c == '\0' || c == '\n' || c == ' ';
 }
 
-int parseInput(char inp[]) {
+int parseOneCommand(char inp[]) {
     char tmp[200], *words[100];                            
     int ix = 0, w = 0;
     int wordlen;
@@ -66,4 +64,31 @@ int parseInput(char inp[]) {
     }
     errorCode = interpreter(words, w);
     return errorCode;
+
+}
+
+int parseInput(char inp[]) {
+    int lastError;
+    char lineCopy[1001];
+    char *cmd;
+    int count;
+
+    lastError = 0;
+    
+    // work on copy bc strtok modifies 
+    strncpy(lineCopy, inp, 1000);
+    lineCopy[1000] = '\0';
+
+    // splits on semi colons
+    cmd = strtok(lineCopy, ";");
+    count = 0;
+
+    // assume @ most 10 chained instructions
+    while (cmd != NULL && count < 10) { 
+        lastError = parseOneCommand(cmd);
+        cmd = strtok(NULL, ";");
+        count++;
+    }
+
+    return lastError;
 }
